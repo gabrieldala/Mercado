0e9bef65cd1787881993731c9929a5707c40010b
gabrieldala <106240311+gabrieldala@users.noreply.github.com>
2023-09-09 09:38:00 +0100

    Add files via upload
    M v2

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ import express from 'express';
 const app = express();
 
 
+// AQCHIVE ESTATIC
+app.use(express.static('public'));
+
+//VIEW ENGINE
+app.set("view engine", 'ejs');
+
+
 // ROUTAS QUE RENDERIZA A VIEW DO MERCADO EXPRESS
 app.get("/", (req, res) =>{
-    res.send("Mercado Express Funcionando!")
+    res.render("index");
 })
 
 const PORT = 4000
